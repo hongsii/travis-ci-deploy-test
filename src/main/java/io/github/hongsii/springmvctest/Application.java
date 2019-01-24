517d907c403c909c4060bf632770de354e0b7383
Sihong Park <hongsii.dev@gmail.com>
2019-01-25 02:46:56 +0900

    travis 수정

---
+++ src/main/java/io/github/hongsii/springmvctest/Application.java
@@ -0,0 +1,14 @@
+package io.github.hongsii.springmvctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
---
+++ src/main/java/io/github/hongsii/springmvctest/config/WebMvcConfig.java
@@ -0,0 +1,9 @@
+package io.github.hongsii.springmvctest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+public class WebMvcConfig {
+
+}
